1308e0adc116facb2910c78f0413b5fb8027e385
Lina Bousbina <lina.bousbina@epfl.ch>
2023-11-17 00:37:05 +0100

    commit partie bilal good lina en cours

--- helpers.py
+++ helpers.py
@@ -1,6 +1,6 @@
 import gdown as gd
 import os
-
+import json
 
 
 def download_data(file_id, local_file_name):
@@ -16,11 +16,32 @@ def download_data(file_id, local_file_name):
 
     Note:
     - This function checks if the file already exists locally before attempting to download.
-    
+
     """
-    
-    download_link = f'https://drive.google.com/uc?id={file_id}'
+
+    download_link = f"https://drive.google.com/uc?id={file_id}"
     if not os.path.exists(local_file_name):
         gd.download(download_link, local_file_name, quiet=False)
     else:
         print("file already exists")
+
+
+# def extract_year(date_string):
+#     """
+#     Extract the year from a date
+
+#     Parameters:
+#     - date string
+
+#     Returns:
+#     - year string
+#     """
+#     return date_string.split("-")[0] if "-" in date_string else date_string
+
+
+def is_empty_json(json_str):
+    try:
+        json_obj = json.loads(json_str)
+        return isinstance(json_obj, dict) and not bool(json_obj)
+    except (json.JSONDecodeError, TypeError):
+        return False
